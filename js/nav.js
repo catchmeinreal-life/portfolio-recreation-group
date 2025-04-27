bfedfde687327749201b23fe1dc5f5cc650ff6d7
eric matutu <ericmatutu125@gmail.com>
2025-04-27 20:13:01 +0300

    footer and nav bar now rendered dynamically

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,12 @@
 // Navigation Bar
 // Fetch the navigation bar HTML and insert it into the page
 
+
+
 fetch('./pages/nav.html')
     .then(response => response.text())
     .then(data => {
-        document.querySelector('footer').innerHTML = data;
+        document.querySelector('nav').innerHTML = data;
         // Additional code to initialize any footer scripts if necessary
 })
 .catch(error => console.error('Error fetching footer:', error));
